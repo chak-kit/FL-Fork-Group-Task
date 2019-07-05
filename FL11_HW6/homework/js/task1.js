dd5977ce8071f3f849d0f5d744e6e2a3b24df266
aymkin <aymkin@gmail.com>
2019-07-05 18:35:30 +0300

    improve according to the requirments

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -5,14 +5,8 @@ const b2 = prompt('enter y for point B');
 const c1 = prompt('enter x for point C');
 const c2 = prompt('enter y for point C');
 
-const isMidpoint = (a1, a2, b1, b2, c1, c2) => {
-  return (+a1 + +b1) / 2 === +c1 && (+a2 + +b2) / 2 === +c2;
-}
+const DIVIDE_BY_TWO = 2;
 
-console.log('sample test result: ' + isMidpoint(1, 1, 3, 3, 2, 2));
-console.log('sample test result: ' + isMidpoint(1, 5, -4, 5, -1.5, 5));
-console.log('sample test result: ' + isMidpoint(1, 6, -1, -3, 0, 1.5));
-console.log('sample test result: ' + isMidpoint(5, 1, 4, -2, 4.5, -0.5));
-console.log('sample test result: ' + isMidpoint(a1, a2, b1, b2, c1, c2));
+const isMidpoint = (+a1 + +b1) / DIVIDE_BY_TWO === +c1 && (+a2 + +b2) / DIVIDE_BY_TWO === +c2;
 
-alert('user midpoint result: ' + isMidpoint(a1, a2, b1, b2, c1, c2));
+alert('user midpoint result: ' + isMidpoint);
