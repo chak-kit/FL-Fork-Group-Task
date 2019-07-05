2c8ea74293a0ac49d5905ad2ae3b4b596a901a35
aymkin <aymkin@gmail.com>
2019-07-05 18:44:11 +0300

    get rid of the defined fucntions

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -2,24 +2,21 @@ const a = prompt('enter triangles length side A');
 const b = prompt('enter triangles length side B');
 const c = prompt('enter triangles length side C');
 
-const isTriangle = (a, b, c) => {
-  if (
-    +a < +b + +c
-    & +b < +c + +a
-    & +c < +b + +a
-    & +a > 0
-    & +b > 0
-    & +c > 0) {
-    if (a === b & b === c & c === a) {
-      return 'Eequivalent triangle';
-    } else if (a === b || a === c || b === c) {
-      return 'Isosceles triangle';
-    } else {
-      return 'Normal triangle';
-    }
+if (
+  +a < +b + +c
+  & +b < +c + +a
+  & +c < +b + +a
+  & +a > 0
+  & +b > 0
+  & +c > 0) {
+  if (a === b & b === c & c === a) {
+    alert('Eequivalent triangle');
+  } else if (a === b || a === c || b === c) {
+    alert('Isosceles triangle');
   } else {
-    return 'Triangle doesn’t exist';
+    alert('Normal triangle');
   }
+} else {
+  alert('Triangle doesn’t exist');
 }
 
-alert(isTriangle(a, b, c));
