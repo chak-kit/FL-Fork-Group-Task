71355b8c18dd6982d25ee18dcc749c7f0063b565
aymkin <aymkin@gmail.com>
2019-07-16 13:46:09 +0300

    done task 5 HW 9

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,3 +1,16 @@
+let magicNum = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9
+}
+
 // 0. Write function, which returns array of numbers from string parameter
 
 const getNumbers = str => {
@@ -18,7 +31,6 @@ console.log(getNumbers('n1um3ber95'));
 // that have different data types (string, number, boolean, etc.) and returns an object 
 // where keys are data types of received parameters and value are their count. 
 
-const FIVE = 5;
 const findTypes = (...arg) => {
   let obj = {};
   for (let i = 0; i < arg.length; i++) {
@@ -36,33 +48,97 @@ const findTypes = (...arg) => {
 }
 
 console.log(findTypes('number'));
-console.log(findTypes(null, FIVE, 'hello', true));
+console.log(findTypes(null, magicNum.five, 'hello', true));
 
 // 2. Write function, which iterates over array and executes function on each element.
 const executeforEach = (array, callback) => {
   for (let i = 0; i < array.length; i++) {
-    console.log(callback(array[i]));
+    callback(array[i]);
   }
 }
 
-// const addOne = x => x + 1;
-// const powTwo = x => x * x;
-// let arr = [5, 2, 3];
-
-// executeforEach(arr, addOne);
-// executeforEach(arr, powTwo);
-// executeforEach(arr, console.log)
-executeforEach([1, 2, 3], function (el) {
+executeforEach([magicNum.one, magicNum.two, magicNum.three], function (el) {
   console.log(el);
 });
 
 // 3. Write function, which returns transformed array based on function,
 // which passed as a parameter. Reuse function from task 2.
 
-const mapArray = (thisArg, callback) => {
-  
+const mapArray = (array, callback) => {
+  let transformedArray = [];
+  executeforEach(array, element => {
+    transformedArray.push(callback(element));
+  });
+
+  return transformedArray;
 }
 
-mapArray([2, 5, 8], function(el) { 
-  return el + 3; 
-})
+console.log(mapArray([magicNum.four, magicNum.one, magicNum.nine], x => x + magicNum.three));
+
+// 4. Write function, which returns filtered array based on function, which passed as
+// a parameter. Reuse function from task 2.
+
+const filterArray = (array, callback) => {
+  let filtered = [];
+  executeforEach(array, element => {
+    if (callback(element)) {
+      filtered.push(element);
+    }
+  })
+  return filtered;
+}
+
+console.log(filterArray([magicNum.two, magicNum.five, magicNum.eight], (el) => el > magicNum.three));
+
+// 5. Write function, which returns formatted date.
+// showFormattedDate(new Date('2019-01-27T01:10:00')) 
+// returns ‘Date: Jan 27 2019’
+// every month should be showed as 3 letters (e.g. Feb, Apr or Dec)
+
+const showFormattedDate = date => {
+  let month = date.getMonth();
+  console.log(month);
+  switch (month) {
+    case 0:
+      month = 'Jan';
+      break;
+    case 1:
+      month = 'Feb';
+      break;
+    case magicNum.two:
+      month = 'Mar';
+      break;
+    case magicNum.three:
+      month = 'Apr';
+      break;
+    case magicNum.four:
+      month = 'May';
+      break;
+    case magicNum.five:
+      month = 'Jun';
+      break;
+    case magicNum.six:
+      month = 'Jul';
+      break;
+    case magicNum.seven:
+      month = 'Aug';
+      break;
+    case magicNum.eight:
+      month = 'Sep';
+      break;
+    case magicNum.nine:
+      month = 'Oct';
+      break;
+    case magicNum.nine + magicNum.one:
+      month = 'Nov';
+      break;
+    case magicNum.nine + magicNum.two:
+      month = 'Dec';
+      break;
+    default: undefined;
+  }
+
+  return `${month} ${date.getDate()} ${date.getFullYear()}`
+}
+
+console.log(showFormattedDate(new Date('2019-01-27T01:10:00')));
